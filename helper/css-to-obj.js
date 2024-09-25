639cd3e42a849548b9cf0c9855e7b83487e016f4
Ermiyas Arage <152753390+ermi111@users.noreply.github.com>
2024-09-25 10:32:20 +0300

    Format indentation

--- helper/css-to-obj.js
+++ helper/css-to-obj.js
@@ -7,9 +7,9 @@ export function cssToObject(cssString) {
     let match;
     
     while ((match = regex.exec(cssString)) !== null) {
-      const selector = match[1].trim(); // Extract selector
-      const rules = match[2].replace(/\s+/g, ' ').trim(); // Remove newlines and extra spaces
-      cssObject[selector] = rules;
+        const selector = match[1].trim(); // Extract selector
+        const rules = match[2].replace(/\s+/g, ' ').trim(); // Remove newlines and extra spaces
+        cssObject[selector] = rules;
     }
     
     return cssObject;
